6baed0603fa2399fc0a1f250a430f38cf487d396
joel.buti97@gmail.com <joel.buti97@gmail.com>
2021-02-03 01:50:39 +0200

    Added API's

--- routers/admin.js
+++ routers/admin.js
@@ -18,43 +18,36 @@ router.get('/viewAllAppointment', function (req, res, next) {
 
 });
 
-router.delete('/deleteAppointment/:id', function (req, res, next){
-    connection.query('delete from appointment where appId =?',req.params.id, function(error,results){
-        if(!error){
-            console.log('Appointment with ID ',req.params.id,' is Canceled');
-            res.send(`Appointment with ID ${[req.params.id]} is Canceled`);
+/*
+router.delete('/deleteAppointment/:id', function (req, res, next) {
+    connection.query('delete from appointment where appId =?', req.params.id, function (error) {
+        if (!error) {
+            console.log('Appointment with ID ', req.params.id, ' is Canceled');
+            res.send(`Appointment is Canceled`);
         }
-        else{
+        else {
             console.log(error);
             res.send(error);
         }
     })
 })
+*/
+router.put('/updateAppointment', function (req, res, next) {
 
-router.put('/updateAppointment', function (req, res, next){
+    const params = req.body;
 
-    const doctor = req.body.doctorId;
-    const isattend = req.body.isAttend;
-    const appId = req.body.appId;
+    connection.query('update appointment set isDeleted=?, isAttend=?, appLocation=? where appId =?', [params.isDeleted, params.isAttend, params.appLocation, params.appId], function (error, results) {
+        if (!error) {
 
-
-
-    connection.query('update appointment set doctorId=?, isAttend=? where appId =?',[doctor,isattend,appId], function(error,results){
-        if(!error){
-            console.log('Appointment with ID ',appId,' is Updated');
-            res.send(`Appointment with ID ${[appId]} is Updated`);
-            console.log(results)
+            console.log('Appointment with ID ', params.appId, ' is Updated');
+            res.send(`Appointment with ID ${[params.appId]} is Updated`);
+            console.log(params)
         }
-        else{
+        else {
             console.log(error);
             res.send(error);
         }
     })
 })
-
-
-
-
-
 
 module.exports = router
